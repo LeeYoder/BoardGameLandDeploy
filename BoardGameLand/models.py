77996d2cd94a038a24ab4baff034395e7e9a5894
LeeYoder <lwyoder807@gmail.com>
2021-06-25 22:02:50 -0500

    Sprint 2

--- BoardGameLand/models.py
+++ BoardGameLand/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 
 class Genre(models.Model):
     """Model representing a game genre."""
@@ -12,12 +12,36 @@ class Genre(models.Model):
 
 class Game(models.Model):
     """Model representing a game."""
-    name = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     description = models.TextField(max_length=1000, help_text='Enter a brief description of the game')
     price = models.DecimalField(max_digits=5, decimal_places=2)
 
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
 
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this game."""
+        return reverse('game', args=[str(self.id)])
+
     def __str__(self):
         """String for representing the Model object."""
         return self.title
+
+class User(models.Model):
+    """Model representing a user."""
+    given_name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    address = models.CharField(max_length=75)
+
+    def __str__(self):
+        """String for representing the Model object"""
+        return self.surname + ", " + self.given_name
+
+class Cart(models.Model):
+    """Model representing a cart."""
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    items = models.ManyToManyField(Game, help_text="list of items in user's shopping cart.")
+
+    def __str__(self):
+        """String for representing the Model object"""
+        return self.items
--- BoardGameLand/views.py
+++ BoardGameLand/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render #, get_object_or_404
+from BoardGameLand.models import *
+#from django.db.models import Sum
+from django.views import generic
 
 # Create your views here.
 
 def home(request):
     return render(request, 'homepage.html')
+
+class GameList(generic.ListView):
+    model = Game
+    paginate_by = 9
+
+class Game(generic.DetailView):
+    model = Game
+
+@login_required
+def cart(request, pk):
+    #user = get_object_or_404(User, pk=pk)
+    items = Cart.objects.filter(user_id=pk)
+    total_price = 0
+    for item in items:
+        total_price += item.price
+    return render(request, 'BoardGameLand/cart.html', {#'user': user,
+                                                       'items': items,
+                                                       'total_price': total_price})
